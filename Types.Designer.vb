24a0af30307cc70da076d231cdc183b2e673c569
Matias Marceca <Matias Marceca>
2018-10-19 02:33:25 -0300

    Mods:    - New behavior when 'new' button is pressed by the user    - No New Empty rows created before save a new entry  Committer: Matias Marceca <Matias Marceca>
     On branch fixing_types_form
     Changes to be committed:
    	modified:   Types.Designer.vb
    	modified:   Types.vb
    	modified:   TypesDao.vb

--- Types.Designer.vb
+++ Types.Designer.vb
@@ -22,173 +22,199 @@ Partial Class Types
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.btn_clean = New System.Windows.Forms.Button()
-        Me.btn_delete = New System.Windows.Forms.Button()
-        Me.btn_update = New System.Windows.Forms.Button()
-        Me.btn_prev = New System.Windows.Forms.Button()
-        Me.btn_next = New System.Windows.Forms.Button()
-        Me.btn_new = New System.Windows.Forms.Button()
-        Me.btn_exit = New System.Windows.Forms.Button()
-        Me.drp_types = New System.Windows.Forms.ComboBox()
-        Me.Label6 = New System.Windows.Forms.Label()
-        Me.txt_name = New System.Windows.Forms.TextBox()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.txt_codint = New System.Windows.Forms.TextBox()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.lab_type_error_msg = New System.Windows.Forms.Label()
-        Me.SuspendLayout()
-        '
-        'btn_clean
-        '
-        Me.btn_clean.Location = New System.Drawing.Point(98, 174)
-        Me.btn_clean.Name = "btn_clean"
-        Me.btn_clean.Size = New System.Drawing.Size(75, 23)
-        Me.btn_clean.TabIndex = 11
-        Me.btn_clean.Text = "Limpiar"
-        Me.btn_clean.UseVisualStyleBackColor = True
-        '
-        'btn_delete
-        '
-        Me.btn_delete.Location = New System.Drawing.Point(193, 174)
-        Me.btn_delete.Name = "btn_delete"
-        Me.btn_delete.Size = New System.Drawing.Size(75, 23)
-        Me.btn_delete.TabIndex = 12
-        Me.btn_delete.Text = "Eliminar"
-        Me.btn_delete.UseVisualStyleBackColor = True
-        '
-        'btn_update
-        '
-        Me.btn_update.Location = New System.Drawing.Point(4, 174)
-        Me.btn_update.Name = "btn_update"
-        Me.btn_update.Size = New System.Drawing.Size(75, 23)
-        Me.btn_update.TabIndex = 10
-        Me.btn_update.Text = "Actualizar"
-        Me.btn_update.UseVisualStyleBackColor = True
-        '
-        'btn_prev
-        '
-        Me.btn_prev.Location = New System.Drawing.Point(193, 136)
-        Me.btn_prev.Name = "btn_prev"
-        Me.btn_prev.Size = New System.Drawing.Size(75, 23)
-        Me.btn_prev.TabIndex = 9
-        Me.btn_prev.Text = "Anterior"
-        Me.btn_prev.UseVisualStyleBackColor = True
-        '
-        'btn_next
-        '
-        Me.btn_next.Location = New System.Drawing.Point(98, 136)
-        Me.btn_next.Name = "btn_next"
-        Me.btn_next.Size = New System.Drawing.Size(75, 23)
-        Me.btn_next.TabIndex = 8
-        Me.btn_next.Text = "Sigiente"
-        Me.btn_next.UseVisualStyleBackColor = True
-        '
-        'btn_new
-        '
-        Me.btn_new.Location = New System.Drawing.Point(4, 136)
-        Me.btn_new.Name = "btn_new"
-        Me.btn_new.Size = New System.Drawing.Size(75, 23)
-        Me.btn_new.TabIndex = 7
-        Me.btn_new.Text = "Nuevo"
-        Me.btn_new.UseVisualStyleBackColor = True
-        '
-        'btn_exit
-        '
-        Me.btn_exit.Location = New System.Drawing.Point(98, 226)
-        Me.btn_exit.Name = "btn_exit"
-        Me.btn_exit.Size = New System.Drawing.Size(75, 23)
-        Me.btn_exit.TabIndex = 13
-        Me.btn_exit.Text = "Salir"
-        Me.btn_exit.UseVisualStyleBackColor = True
-        '
-        'drp_types
-        '
-        Me.drp_types.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
-        Me.drp_types.FormattingEnabled = True
-        Me.drp_types.Location = New System.Drawing.Point(89, 12)
-        Me.drp_types.Name = "drp_types"
-        Me.drp_types.Size = New System.Drawing.Size(179, 21)
-        Me.drp_types.TabIndex = 1
-        '
-        'Label6
-        '
-        Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(15, 15)
-        Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(60, 13)
-        Me.Label6.TabIndex = 61
-        Me.Label6.Text = "Elegir Tipo:"
-        '
-        'txt_name
-        '
-        Me.txt_name.Location = New System.Drawing.Point(89, 50)
-        Me.txt_name.MaxLength = 90
-        Me.txt_name.Name = "txt_name"
-        Me.txt_name.Size = New System.Drawing.Size(179, 20)
-        Me.txt_name.TabIndex = 2
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(19, 57)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(47, 13)
-        Me.Label1.TabIndex = 60
-        Me.Label1.Text = "Nombre:"
-        '
-        'txt_codint
-        '
-        Me.txt_codint.Location = New System.Drawing.Point(89, 86)
-        Me.txt_codint.MaxLength = 10
-        Me.txt_codint.Name = "txt_codint"
-        Me.txt_codint.Size = New System.Drawing.Size(179, 20)
-        Me.txt_codint.TabIndex = 3
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(19, 93)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(58, 13)
-        Me.Label3.TabIndex = 63
-        Me.Label3.Text = "Codigo Int:"
-        '
-        'lab_type_error_msg
-        '
-        Me.lab_type_error_msg.AutoSize = True
-        Me.lab_type_error_msg.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle))
-        Me.lab_type_error_msg.ForeColor = System.Drawing.Color.Crimson
-        Me.lab_type_error_msg.Location = New System.Drawing.Point(92, 110)
-        Me.lab_type_error_msg.Name = "lab_type_error_msg"
-        Me.lab_type_error_msg.Size = New System.Drawing.Size(143, 13)
-        Me.lab_type_error_msg.TabIndex = 64
-        Me.lab_type_error_msg.Text = "Error: Codigo Incorrecto"
-        '
-        'Types
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
-        Me.Controls.Add(Me.lab_type_error_msg)
-        Me.Controls.Add(Me.txt_codint)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.drp_types)
-        Me.Controls.Add(Me.Label6)
-        Me.Controls.Add(Me.txt_name)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.btn_clean)
-        Me.Controls.Add(Me.btn_delete)
-        Me.Controls.Add(Me.btn_update)
-        Me.Controls.Add(Me.btn_prev)
-        Me.Controls.Add(Me.btn_next)
-        Me.Controls.Add(Me.btn_new)
-        Me.Controls.Add(Me.btn_exit)
-        Me.Name = "Types"
-        Me.Text = "Types"
-        Me.ResumeLayout(False)
-        Me.PerformLayout()
+    Me.btn_clean = New System.Windows.Forms.Button()
+    Me.btn_delete = New System.Windows.Forms.Button()
+    Me.btn_update = New System.Windows.Forms.Button()
+    Me.btn_prev = New System.Windows.Forms.Button()
+    Me.btn_next = New System.Windows.Forms.Button()
+    Me.btn_new = New System.Windows.Forms.Button()
+    Me.btn_exit = New System.Windows.Forms.Button()
+    Me.drp_types = New System.Windows.Forms.ComboBox()
+    Me.Label6 = New System.Windows.Forms.Label()
+    Me.txt_name = New System.Windows.Forms.TextBox()
+    Me.Label1 = New System.Windows.Forms.Label()
+    Me.txt_codint = New System.Windows.Forms.TextBox()
+    Me.Label3 = New System.Windows.Forms.Label()
+    Me.lab_type_error_msg = New System.Windows.Forms.Label()
+    Me.btn_save = New System.Windows.Forms.Button()
+    Me.btn_cancel = New System.Windows.Forms.Button()
+    Me.SuspendLayout()
+    '
+    'btn_clean
+    '
+    Me.btn_clean.Location = New System.Drawing.Point(98, 174)
+    Me.btn_clean.Name = "btn_clean"
+    Me.btn_clean.Size = New System.Drawing.Size(75, 23)
+    Me.btn_clean.TabIndex = 8
+    Me.btn_clean.Text = "Limpiar"
+    Me.btn_clean.UseVisualStyleBackColor = True
+    '
+    'btn_delete
+    '
+    Me.btn_delete.Location = New System.Drawing.Point(193, 174)
+    Me.btn_delete.Name = "btn_delete"
+    Me.btn_delete.Size = New System.Drawing.Size(75, 23)
+    Me.btn_delete.TabIndex = 9
+    Me.btn_delete.Text = "Eliminar"
+    Me.btn_delete.UseVisualStyleBackColor = True
+    '
+    'btn_update
+    '
+    Me.btn_update.Location = New System.Drawing.Point(4, 174)
+    Me.btn_update.Name = "btn_update"
+    Me.btn_update.Size = New System.Drawing.Size(75, 23)
+    Me.btn_update.TabIndex = 7
+    Me.btn_update.Text = "Actualizar"
+    Me.btn_update.UseVisualStyleBackColor = True
+    '
+    'btn_prev
+    '
+    Me.btn_prev.Location = New System.Drawing.Point(193, 136)
+    Me.btn_prev.Name = "btn_prev"
+    Me.btn_prev.Size = New System.Drawing.Size(75, 23)
+    Me.btn_prev.TabIndex = 6
+    Me.btn_prev.Text = "Anterior"
+    Me.btn_prev.UseVisualStyleBackColor = True
+    '
+    'btn_next
+    '
+    Me.btn_next.Location = New System.Drawing.Point(98, 136)
+    Me.btn_next.Name = "btn_next"
+    Me.btn_next.Size = New System.Drawing.Size(75, 23)
+    Me.btn_next.TabIndex = 5
+    Me.btn_next.Text = "Sigiente"
+    Me.btn_next.UseVisualStyleBackColor = True
+    '
+    'btn_new
+    '
+    Me.btn_new.Location = New System.Drawing.Point(4, 136)
+    Me.btn_new.Name = "btn_new"
+    Me.btn_new.Size = New System.Drawing.Size(75, 23)
+    Me.btn_new.TabIndex = 4
+    Me.btn_new.Text = "Nuevo"
+    Me.btn_new.UseVisualStyleBackColor = True
+    '
+    'btn_exit
+    '
+    Me.btn_exit.Location = New System.Drawing.Point(98, 226)
+    Me.btn_exit.Name = "btn_exit"
+    Me.btn_exit.Size = New System.Drawing.Size(75, 23)
+    Me.btn_exit.TabIndex = 90
+    Me.btn_exit.Text = "Salir"
+    Me.btn_exit.UseVisualStyleBackColor = True
+    '
+    'drp_types
+    '
+    Me.drp_types.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+    Me.drp_types.FormattingEnabled = True
+    Me.drp_types.Location = New System.Drawing.Point(89, 12)
+    Me.drp_types.Name = "drp_types"
+    Me.drp_types.Size = New System.Drawing.Size(179, 21)
+    Me.drp_types.TabIndex = 1
+    '
+    'Label6
+    '
+    Me.Label6.AutoSize = True
+    Me.Label6.Location = New System.Drawing.Point(15, 15)
+    Me.Label6.Name = "Label6"
+    Me.Label6.Size = New System.Drawing.Size(60, 13)
+    Me.Label6.TabIndex = 61
+    Me.Label6.Text = "Elegir Tipo:"
+    '
+    'txt_name
+    '
+    Me.txt_name.Location = New System.Drawing.Point(89, 50)
+    Me.txt_name.MaxLength = 90
+    Me.txt_name.Name = "txt_name"
+    Me.txt_name.Size = New System.Drawing.Size(179, 20)
+    Me.txt_name.TabIndex = 2
+    '
+    'Label1
+    '
+    Me.Label1.AutoSize = True
+    Me.Label1.Location = New System.Drawing.Point(19, 57)
+    Me.Label1.Name = "Label1"
+    Me.Label1.Size = New System.Drawing.Size(47, 13)
+    Me.Label1.TabIndex = 60
+    Me.Label1.Text = "Nombre:"
+    '
+    'txt_codint
+    '
+    Me.txt_codint.Location = New System.Drawing.Point(89, 86)
+    Me.txt_codint.MaxLength = 10
+    Me.txt_codint.Name = "txt_codint"
+    Me.txt_codint.Size = New System.Drawing.Size(179, 20)
+    Me.txt_codint.TabIndex = 3
+    '
+    'Label3
+    '
+    Me.Label3.AutoSize = True
+    Me.Label3.Location = New System.Drawing.Point(19, 93)
+    Me.Label3.Name = "Label3"
+    Me.Label3.Size = New System.Drawing.Size(58, 13)
+    Me.Label3.TabIndex = 63
+    Me.Label3.Text = "Codigo Int:"
+    '
+    'lab_type_error_msg
+    '
+    Me.lab_type_error_msg.AutoSize = True
+    Me.lab_type_error_msg.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle))
+    Me.lab_type_error_msg.ForeColor = System.Drawing.Color.Crimson
+    Me.lab_type_error_msg.Location = New System.Drawing.Point(92, 110)
+    Me.lab_type_error_msg.Name = "lab_type_error_msg"
+    Me.lab_type_error_msg.Size = New System.Drawing.Size(143, 13)
+    Me.lab_type_error_msg.TabIndex = 64
+    Me.lab_type_error_msg.Text = "Error: Codigo Incorrecto"
+    '
+    'btn_save
+    '
+    Me.btn_save.Enabled = False
+    Me.btn_save.Location = New System.Drawing.Point(98, 136)
+    Me.btn_save.Name = "btn_save"
+    Me.btn_save.Size = New System.Drawing.Size(75, 23)
+    Me.btn_save.TabIndex = 20
+    Me.btn_save.Text = "Guardar"
+    Me.btn_save.UseVisualStyleBackColor = True
+    Me.btn_save.Visible = False
+    '
+    'btn_cancel
+    '
+    Me.btn_cancel.Enabled = False
+    Me.btn_cancel.Location = New System.Drawing.Point(193, 136)
+    Me.btn_cancel.Name = "btn_cancel"
+    Me.btn_cancel.Size = New System.Drawing.Size(75, 23)
+    Me.btn_cancel.TabIndex = 21
+    Me.btn_cancel.Text = "Cancelar"
+    Me.btn_cancel.UseVisualStyleBackColor = True
+    Me.btn_cancel.Visible = False
+    '
+    'Types
+    '
+    Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+    Me.ClientSize = New System.Drawing.Size(284, 261)
+    Me.Controls.Add(Me.btn_cancel)
+    Me.Controls.Add(Me.btn_save)
+    Me.Controls.Add(Me.lab_type_error_msg)
+    Me.Controls.Add(Me.txt_codint)
+    Me.Controls.Add(Me.Label3)
+    Me.Controls.Add(Me.drp_types)
+    Me.Controls.Add(Me.Label6)
+    Me.Controls.Add(Me.txt_name)
+    Me.Controls.Add(Me.Label1)
+    Me.Controls.Add(Me.btn_clean)
+    Me.Controls.Add(Me.btn_delete)
+    Me.Controls.Add(Me.btn_update)
+    Me.Controls.Add(Me.btn_prev)
+    Me.Controls.Add(Me.btn_next)
+    Me.Controls.Add(Me.btn_new)
+    Me.Controls.Add(Me.btn_exit)
+    Me.Name = "Types"
+    Me.Text = "Types"
+    Me.ResumeLayout(False)
+    Me.PerformLayout()
 
-    End Sub
+  End Sub
     Friend WithEvents btn_clean As System.Windows.Forms.Button
     Friend WithEvents btn_delete As System.Windows.Forms.Button
     Friend WithEvents btn_update As System.Windows.Forms.Button
@@ -202,5 +228,7 @@ Partial Class Types
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents txt_codint As System.Windows.Forms.TextBox
     Friend WithEvents Label3 As System.Windows.Forms.Label
-    Friend WithEvents lab_type_error_msg As System.Windows.Forms.Label
+  Friend WithEvents lab_type_error_msg As System.Windows.Forms.Label
+  Friend WithEvents btn_save As System.Windows.Forms.Button
+  Friend WithEvents btn_cancel As System.Windows.Forms.Button
 End Class
--- TypesDao.vb
+++ TypesDao.vb
@@ -1,102 +1,108 @@
 ﻿Public NotInheritable Class TypesDao
-    Const TABLE_NAME As String = "types"
-    Private recordList As ArrayList
-    Private fieldsList As Hashtable
-    Private dataBaseManager As DataBaseManager
-    Private Shared typSingleton As TypesDao = Nothing
+  Const TABLE_NAME As String = "types"
+  Private recordList As ArrayList
+  Private fieldsList As Hashtable
+  Private dataBaseManager As DataBaseManager
+  Private Shared typSingleton As TypesDao = Nothing
 
-    Dim da As OleDb.OleDbDataAdapter
-    Dim cb As OleDb.OleDbCommandBuilder
-    Dim dt As DataTable
-    Dim dr As DataRow
-    Dim registerPos As Integer = 0
+  Dim da As OleDb.OleDbDataAdapter
+  Dim cb As OleDb.OleDbCommandBuilder
+  Dim dt As DataTable
+  Dim dr As DataRow
+  Dim registerPos As Integer = 0
 
 
-    Public Shared ReadOnly Property GetInstance()
-        Get
-            If (typSingleton Is Nothing) Then
-                typSingleton = New TypesDao()
-            End If
+  Public Shared ReadOnly Property GetInstance()
+    Get
+      If (typSingleton Is Nothing) Then
+        typSingleton = New TypesDao()
+      End If
 
-            Return typSingleton
-        End Get
-    End Property
+      Return typSingleton
+    End Get
+  End Property
 
-    Public Sub New()
-        recordList = New ArrayList()
-        fieldsList = New Hashtable()
-        dataBaseManager = dataBaseManager.GetInstance()
+  Public Sub New()
+    recordList = New ArrayList()
+    fieldsList = New Hashtable()
+    dataBaseManager = dataBaseManager.GetInstance()
 
-        fieldsList.Add("typ_name", "")
-        fieldsList.Add("codint", "")
+    fieldsList.Add("typ_name", "")
+    fieldsList.Add("codint", "")
 
-        dataBaseManager.PrepareDao(TABLE_NAME, da, cb, dt)
-        FillRecordList()
-    End Sub
+    dataBaseManager.PrepareDao(TABLE_NAME, da, cb, dt)
+    FillRecordList()
+  End Sub
 
-    Private Sub FillRecordList()
-        Dim rowsCount As Integer = 0
-        Dim i As Integer = 0
+  Private Sub FillRecordList()
+    Dim rowsCount As Integer = 0
+    Dim i As Integer = 0
 
-        rowsCount = dt.Rows.Count
-        While i < rowsCount
-            fieldsList = New Hashtable()
-            dr = dt.Rows(i)
-            fieldsList("typ_name") = dr("typ_name")
-            fieldsList("codint") = dr("codint")
-            recordList.Add(fieldsList)
-            i += 1
-        End While
-    End Sub
+    rowsCount = dt.Rows.Count
+    While i < rowsCount
+      fieldsList = New Hashtable()
+      dr = dt.Rows(i)
+      fieldsList("typ_name") = dr("typ_name")
+      fieldsList("codint") = dr("codint")
+      recordList.Add(fieldsList)
+      i += 1
+    End While
+  End Sub
 
-    Public Sub EraseRecord()
-        dataBaseManager.EraseRecord(registerPos, da, dt)
-    End Sub
+  Public Sub EraseRecord()
+    dataBaseManager.EraseRecord(registerPos, da, dt)
+  End Sub
 
-    Public Sub SaveRecord()
-        dr = dt.Rows(registerPos)
-        SaveDataInRecord()
-        dataBaseManager.SubmmitChanges(da, dt)
-    End Sub
+  Public Sub SaveRecord(ByVal isNewRecord As Boolean)
+    Console.WriteLine(" Save Record : register pos : " & registerPos)
 
-    Public Sub CreateNewEmptyRecord()
-        dr = dt.NewRow()
-        dt.Rows.Add(dr)
-        dataBaseManager.SubmmitChanges(da, dt)
-    End Sub
+    If isNewRecord = True Then
+      dr = dt.NewRow()
+      dt.Rows.Add(dr)
+      registerPos = dt.Rows.Count - 1
+    End If
 
-    ' Get Functions
-    Public Function GetFields() As Hashtable
-        Return fieldsList
-    End Function
+    Console.WriteLine(" Save Record 2 : register pos : " & registerPos)
 
-    Public Function GetRecords() As ArrayList
-        Return recordList
-    End Function
+    dr = dt.Rows(registerPos)
+    SaveDataInRecord()
+    dataBaseManager.SubmmitChanges(da, dt)
+  End Sub
 
-    Public Function GetRegisterPos() As Integer
-        Return registerPos
-    End Function
+  ' Get Functions
+  Public Function GetFields() As Hashtable
+    Return fieldsList
+  End Function
 
-    ' Set Functions
-    Public Sub SetRecordList(ByRef records As ArrayList)
-        recordList = records
-    End Sub
+  Public Function GetRecords() As ArrayList
+    Return recordList
+  End Function
 
-    Public Sub SetRegisterPos(ByVal newRegisterPos As Integer)
-        registerPos = newRegisterPos
-    End Sub
+  Public Function GetRegisterPos() As Integer
+    Return registerPos
+  End Function
 
-    ' Privates
-    Private Sub SaveDataInRecord()
-        Dim i As Integer = 0
-        Dim fieldsCount As Integer = 0
+  ' Set Functions
+  Public Sub SetRecordList(ByRef records As ArrayList)
+    recordList = records
+  End Sub
 
-        fieldsCount = fieldsList.Count
-        While i < fieldsCount
-            Console.WriteLine("Element - " & recordList.Item(registerPos)(fieldsList.Keys(i)))
-            dr(fieldsList.Keys(i)) = recordList.Item(registerPos)(fieldsList.Keys(i))
-            i += 1
-        End While
-    End Sub
+  Public Sub SetRegisterPos(ByVal newRegisterPos As Integer)
+    registerPos = newRegisterPos
+  End Sub
+
+  ' Privates
+  Private Sub SaveDataInRecord()
+    Dim i As Integer = 0
+    Dim fieldsCount As Integer = 0
+
+    Console.WriteLine(" Save Data in Record : register pos : " & registerPos)
+
+    fieldsCount = fieldsList.Count
+    While i < fieldsCount
+      Console.WriteLine("Element - " & recordList.Item(registerPos)(fieldsList.Keys(i)))
+      dr(fieldsList.Keys(i)) = recordList.Item(registerPos)(fieldsList.Keys(i))
+      i += 1
+    End While
+  End Sub
 End Class
