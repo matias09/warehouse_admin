93976ab702d76c93b534fb4443ca0cb49c39dd22
Matias Marceca <Matias Marceca>
2018-10-19 15:31:54 -0300

    Adds:   - Regex support cross Classes using Utils.  Committer: Matias Marceca <Matias Marceca>
     On branch master
     Changes to be committed:
    	modified:   Utils.vb

--- Utils.vb
+++ Utils.vb
@@ -1,56 +1,62 @@
-﻿Public Class Utils
-    Public Sub ChangeFocusForm(ByRef actualForm As Form, ByVal toForm As String)
-        actualForm.Hide()
-        'If (toForm = "Products") Then
-        '    Products.Show()
-        '    Products.Focus()
-        'ElseIf (toForm = "Types") Then
-        '    Types.Show()
-        '    Types.Focus()
-        'ElseIf (toForm = "") Then
-        '    .Show()
-        '    .Focus()
-        'ElseIf (toForm = "") Then
-        '    .Show()
-        '    .Focus()
-        'End If
+﻿Imports System.Text.RegularExpressions
 
-        If (toForm = "Types") Then
-            Types.Show()
-            Types.Focus()
-        End If
-    End Sub
+Public Class Utils
+  Public Sub ChangeFocusForm(ByRef actualForm As Form, ByVal toForm As String)
+    actualForm.Hide()
+    If (toForm = "Products") Then
+      'Products.Show()
+      'Products.Focus()
+    ElseIf (toForm = "Types") Then
+      Types.Show()
+      Types.Focus()
+    ElseIf (toForm = "Sector") Then
+      'Sector.Show()
+      'Sector.Focus()
+    ElseIf (toForm = "") Then
+      '.Show()
+      '.Focus()
+    End If
+  End Sub
 
-    Public Sub ResetControls(ByRef form As Form)
-        Dim cControl As Control
-        For Each cControl In form.Controls
-            If (TypeOf cControl Is TextBox) Then
-                cControl.Text = ""
-            End If
-        Next cControl
-    End Sub
+  Public Sub ResetControls(ByRef form As Form)
+    Dim cControl As Control
+    For Each cControl In form.Controls
+      If (TypeOf cControl Is TextBox) Then
+        cControl.Text = ""
+      End If
+    Next cControl
+  End Sub
 
-    Public Sub EnableControls(ByRef form As Form)
-        Dim cControl As Control
-        For Each cControl In form.Controls
-            cControl.Enabled = True
-        Next cControl
-    End Sub
+  Public Sub EnableControls(ByRef form As Form)
+    Dim cControl As Control
+    For Each cControl In form.Controls
+      cControl.Enabled = True
+    Next cControl
+  End Sub
 
-    Public Sub DisableControls(ByRef form As Form)
-        Dim cControl As Control
-        For Each cControl In form.Controls
-            cControl.Enabled = False
-        Next cControl
-    End Sub
+  Public Sub DisableControls(ByRef form As Form)
+    Dim cControl As Control
+    For Each cControl In form.Controls
+      cControl.Enabled = False
+    Next cControl
+  End Sub
 
-    Public Function UpdateRegisterPos(ByVal futRegPos As Integer, ByVal eleRecordsCount As Integer) As Integer
-        If (futRegPos = eleRecordsCount) Then
-            futRegPos = 0
-        ElseIf (futRegPos < 0) Then
-            futRegPos = eleRecordsCount - 1
-        End If
+  Public Function UpdateRegisterPos(ByVal futRegPos As Integer, ByVal eleRecordsCount As Integer) As Integer
+    If (futRegPos = eleRecordsCount) Then
+      futRegPos = 0
+    ElseIf (futRegPos < 0) Then
+      futRegPos = eleRecordsCount - 1
+    End If
 
-        Return futRegPos
-    End Function
+    Return futRegPos
+  End Function
+
+  Public Function CheckExpressionByPatternMatching(ByRef str As String, ByRef pattern As String) As Boolean
+    ' Check is the given str match with the pattern provided
+    If Regex.IsMatch(str, pattern) = True Then
+      Return True
+    End If
+
+    Return False
+  End Function
 End Class
